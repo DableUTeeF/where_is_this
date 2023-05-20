8af10b2ee8a7b7e3dff60e2b95f57f5d4274219b
Natthaphon Hongcharoen <palm22180@gmail.com>
2023-05-20 16:33:48 +0700

    Added python hamming distance

--- hamming_distance_old.py
+++ hamming_distance_old.py
@@ -1,4 +1,17 @@
 import numpy as np
 import hamming_distance
 
-print(hamming_distance.hamming_distance(np.array([2]), np.array(['001'], dtype=np.ubyte), np.array(['101'], dtype=np.ubyte)))
+print(
+    hamming_distance.hamming_distance(
+        np.array([3]),
+        np.array(['001'], dtype=np.ubyte),
+        np.array(['101'], dtype=np.ubyte))
+)
+
+print(
+    hamming_distance.hamming_distance(
+        3,
+        '011',
+        '001'
+    )
+)
